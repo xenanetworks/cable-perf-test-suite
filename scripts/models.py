09d5e00ddbaa4c9477f722d337d7925eb6ddfe7f
Leonard Yu <leonard.yu@teledyne.com>
2025-02-05 18:48:02 +0100

    Make target test into class

--- scripts/models.py
+++ scripts/models.py
@@ -34,3 +34,24 @@ class CablePerfOptimalTestConfig(BaseModel):
     delay_after_reset: int
     prbs_test_config: PRBSTestConfig
     transceiver_eq_config: TransceiverEQConfig
+
+class TransceiverInitConfig(BaseModel):
+    amp_init: int
+    pre_init: int
+    post_init: int
+    target_prbs_ber: float
+    delay_after_eq_write: int
+
+class CablePerfTargetTestConfig(BaseModel):
+    chassis_ip: str
+    username: str
+    password: str
+    tcp_port: int
+    log_filename: str
+    port_pair_list: list[PortPair]
+    module_media: str
+    port_speed: str
+    lane: int
+    delay_after_reset: int
+    prbs_test_config: PRBSTestConfig
+    transceiver_init_config: TransceiverInitConfig
